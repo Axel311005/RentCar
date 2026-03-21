4d540b967a1a3df5af244cbcd0b52ea0f9e54caf
Axel Perez <aperez@MBA01s-MacBook-Air.local>
2026-03-20 20:34:35 -0600

    add: cors frontend vercel

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:3001', 'https://rent-car-frontend-brown.vercel.app'],
     credentials: true,
   });
   app.useGlobalPipes(
